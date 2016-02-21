58f0eac5636b7917b0b9b17db555d7b84a2b292d
Riley Schuit <riley.schuit@gmail.com>
2016-02-20 17:07:56 -0800

    disabled karma test

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,7 +78,7 @@ module.exports = function (grunt) {
           '<%= yeoman.client %>/{app,components}/**/*.spec.js',
           '<%= yeoman.client %>/{app,components}/**/*.mock.js'
         ],
-        tasks: ['newer:jshint:all', 'karma']
+        //tasks: ['newer:jshint:all', 'karma']
       },
       gruntfile: {
         files: ['Gruntfile.js']
@@ -413,12 +413,13 @@ module.exports = function (grunt) {
     },
 
     // Test settings
-    karma: {
+    /*karma: {
       unit: {
         configFile: 'karma.conf.js',
         singleRun: true
       }
     },
+    */
 
     mochaTest: {
       options: {
@@ -563,7 +564,7 @@ module.exports = function (grunt) {
         'concurrent:test',
         'injector',
         'autoprefixer',
-        'karma'
+        //'karma'
       ]);
     }
 
